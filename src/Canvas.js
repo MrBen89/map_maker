98ab3d47a29ce3d1931dce8fd36e21f022d16e3e
Ben Pearson <b.pearson89@hotmail.com>
2024-04-12 11:43:11 +0900

    First Commit

---
+++ src/Canvas.js
@@ -0,0 +1,85 @@
+import { useRef, useEffect } from 'react';
+
+let example_tile=[
+    [0,0,1,1,1,1,0,0],
+    [0,0,1,1,1,1,0,0],
+    [0,0,1,1,1,1,0,0],
+    [0,0,1,1,1,1,0,0],
+    [0,0,1,1,1,1,0,0],
+    [0,0,1,1,1,1,0,0],
+    [0,0,1,1,1,1,0,0],
+    [0,0,1,1,1,1,0,0],
+];
+
+let blank_tile=[
+    [0,0,0,0,0,0,0,0],
+    [0,0,0,0,0,0,0,0],
+    [0,0,0,0,0,0,0,0],
+    [0,0,0,0,0,0,0,0],
+    [0,0,0,0,0,0,0,0],
+    [0,0,0,0,0,0,0,0],
+    [0,0,0,0,0,0,0,0],
+    [0,0,0,0,0,0,0,0],
+]
+
+let zoom = 5;
+
+  
+
+export function Canvas(props) {
+  const canvasRef = useRef(null);
+
+  useEffect(() => {
+    
+    const canvas = canvasRef.current;
+    const context = canvas.getContext('2d');
+
+    function drawGrid() {
+        for (let x = 0; x < canvas.width; x += (8*zoom)){
+            context.beginPath();
+            context.moveTo(x,0);
+            context.lineTo(x,canvas.width);
+            context.strokeStyle = "#888";
+            context.stroke();
+        }
+        for (let y = 0; y < canvas.height; y += (8*zoom)){
+            context.beginPath();
+            context.moveTo(0,y);
+            context.lineTo(canvas.height,y);
+            context.strokeStyle = "#888";
+            context.stroke();
+        }
+    }
+
+    
+
+    function drawTile(tile, locationX, locationY) {
+        for (let x = 0; x < 8; x++) {
+          for (let y = 0; y < 8; y++){
+            if (tile[y][x] == 0){
+              context.fillStyle = "#000";
+              context.fillRect(locationX + (zoom * x), locationY + (zoom * y), zoom, zoom);
+            } else {
+              context.fillStyle = "#FFF";
+              context.fillRect(locationX + (zoom * x), locationY + (zoom * y), zoom, zoom);
+            }
+          }
+          
+        }
+    };
+
+    function blankScreen() {
+        for (let x = 0; x < canvas.width; x += (8*zoom)) {
+            for (let y = 0; y < canvas.height; y += (8*zoom)) {
+                drawTile(blank_tile, x, y);
+            }
+        }
+    }
+    blankScreen();
+    drawGrid();
+  }, []);
+
+  return <canvas ref={canvasRef} width='3000' height='3200'/>
+}
+
+
